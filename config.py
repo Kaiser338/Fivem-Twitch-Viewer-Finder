4f3b338767a0c54495fa95fb95f7de615526b3cd
Kaiser338 <46496227+Kaiser338@users.noreply.github.com>
2024-02-04 22:02:50 +0100

    upt

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 DATABASE_CONFIG = {
-    'HOST': "127.0.0.1",
-    'USER': "root",
-    'PASSWORD': "gLp24VNX2x",
-    'DATABASE': "twitch_players",
-    'PORT': 3307
+    'HOST': ",
+    'USER': "",
+    'PASSWORD': "",
+    'DATABASE': "",
+    'PORT': 3306
 }
 
 WEBHOOK_CONFIG = {
-    'URL': "https://discord.com/api/webhooks/902592100883906600/cxAk7nwi77M4bvly8wEjdiCD7jdeTy2yPwGsmWG2QbEdBB7K3a6VqfXlRFQDqfl09Si3",
-    'NAME': "Botek",
-    'AVATAR_URL': "https://banner2.cleanpng.com/20180329/jbe/kisspng-robotics-photography-robots-5abc84222d4ca0.3742717415223040341856.jpg"
+    'URL': "",
+    'NAME': "",
+    'AVATAR_URL': ""
 }
